30bb8b6d6cd26119062ffba8d6f7e5907feec7c5
Kulakov619 <kulakov_i_v@school619.ru>
2022-10-09 20:52:52 +0300

    1

--- config/urls.py
+++ config/urls.py
@@ -5,15 +5,7 @@ from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('openapi', get_schema_view(
-            title="dm_backend",
-            authentication_classes=[],
-            permission_classes=[],
-            version="1.0"
-        ), name='openapi-schema'),
     path('', lambda request: redirect('docs/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/auth/', include('authcore.urls')),
-    re_path(r'^docs/', include_docs_urls(title='dm_backend', public=True)),
-
 ]
